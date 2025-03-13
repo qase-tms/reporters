3ab073d07708e457e7931a3f11b51dbf014ebda7
Vikas Naithani <vksnaithani@gmail.com>
2025-03-13 08:43:45 +0530

    Made changes on syntax page for qase.id

--- src/js/code/jest/id.test.js
+++ src/js/code/jest/id.test.js
@@ -1,10 +1,10 @@
-//ANCHOR:syntax
-const { qase } = require("jest-qase-reporter/jest");
 
-describe("Example: id.test.js", () => {
+const { qase } = require("jest-qase-reporter/jest");
+//ANCHOR:syntax
+// describe("Example: id.test.js", () => {
  // // Please, change the Id from `1` to any case Id present in your project before uncommenting the test.
   test(qase(1, "A test with Qase Id"), () => {
-    expect(true).toBe(true);
-  });
-});
+    // Your test logic here
+ // });
+// });
 //ANCHOR_END:syntax
